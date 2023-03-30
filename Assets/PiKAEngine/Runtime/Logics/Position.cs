ca80e073768acdcad86f3b55ea187c380abede83
KYapp <kukiyomenaisapp@gmail.com>
2023-03-30 20:58:02 +0900

    Position

---
+++ Assets/PiKAEngine/Runtime/Logics/Position.cs
@@ -0,0 +1,32 @@
+namespace JuhaKurisu.PiKAEngine.Logics
+{
+    public struct Position
+    {
+        public readonly ChunkPosition chunkPosition;
+        public readonly TilePosition tilePosition;
+
+        public Position(ChunkPosition chunkPosition, TilePosition tilePosition)
+        {
+            this.chunkPosition = chunkPosition;
+            this.tilePosition = tilePosition;
+        }
+
+        public Position(int chunkX, int chunkY, int tileX, int tileY)
+        {
+            this.chunkPosition = new(chunkX, chunkY);
+            this.tilePosition = new(tileX, tileY);
+        }
+
+        public Position(int chunkX, int chunkY, TilePosition tilePosition)
+        {
+            this.chunkPosition = new(chunkX, chunkY);
+            this.tilePosition = tilePosition;
+        }
+
+        public Position(ChunkPosition chunkPosition, int tileX, int tileY)
+        {
+            this.chunkPosition = chunkPosition;
+            this.tilePosition = new(tileX, tileY);
+        }
+    }
+}
