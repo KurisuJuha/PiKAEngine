4575499f803fc72b4082f121c90f0234f5d8b55d
KYapp <kukiyomenaisapp@gmail.com>
2023-03-30 20:55:32 +0900

    ChunkPosition

---
+++ Assets/PiKAEngine/Runtime/Logics/ChunkPosition.cs
@@ -0,0 +1,14 @@
+namespace JuhaKurisu.PiKAEngine.Logics
+{
+    public struct ChunkPosition
+    {
+        public readonly int x;
+        public readonly int y;
+
+        public ChunkPosition(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
