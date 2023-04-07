e0f57908a64fb920b6ed8cc6f31faf9a3ee65835
KYapp <kukiyomenaisapp@gmail.com>
2023-04-07 19:39:57 +0900

    TileシステムからIDを削除

--- Assets/PiKAEngine/Runtime/Logics/Maps/Tile.cs
+++ Assets/PiKAEngine/Runtime/Logics/Maps/Tile.cs
@@ -6,15 +6,13 @@ namespace JuhaKurisu.PiKAEngine.Logics.Maps
 {
     public class Tile : IDisposable
     {
-        public readonly string id;
         public readonly Position position;
         public readonly ReadOnlyCollection<TileComponent> components;
         public IObservable<Tile> onTileChanged => onTileChangedSubject;
         private readonly Subject<Tile> onTileChangedSubject = new();
 
-        public Tile(string id, Position position, TileComponent[] components)
+        public Tile(Position position, TileComponent[] components)
         {
-            this.id = id;
             this.position = position;
             this.components = new(components);
             foreach (var component in components)
--- Assets/PiKAEngine/Runtime/Logics/Maps/TileContents.cs
+++ Assets/PiKAEngine/Runtime/Logics/Maps/TileContents.cs
@@ -7,20 +7,16 @@ namespace JuhaKurisu.PiKAEngine.Logics.Maps
     [HideMonoScript, CreateAssetMenu(menuName = "PiKAEngine/Tile", fileName = "newTile")]
     public class TileContents : ScriptableObject
     {
-        public string id => _id;
-        [SerializeField, DisableInPlayMode] private string _id;
         [SerializeField, SerializeReference] private TileComponent[] components;
 
-        public TileContents(string id, TileComponent[] components)
+        public TileContents(TileComponent[] components)
         {
-            this._id = id;
             this.components = components;
         }
 
         public Tile GenerateTile(Position position)
         {
             return new Tile(
-                id,
                 position,
                 components.Select(component => component.Copy()).ToArray()
             );
