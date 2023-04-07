637d799de131f26372d2b97b3c7868d347b8f448
KYapp <kukiyomenaisapp@gmail.com>
2023-04-07 16:26:35 +0900

    Tile（コンポーネント）に変更があった場合呼ばれる処理を追加

--- Assets/PiKAEngine/Runtime/Logics/Tile.cs
+++ Assets/PiKAEngine/Runtime/Logics/Tile.cs
@@ -1,14 +1,18 @@
+using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using UniRx;
 
 namespace JuhaKurisu.PiKAEngine.Logics
 {
-    public class Tile
+    public class Tile : IDisposable
     {
         public string id { get; private set; }
         public Position position;
         public ReadOnlyCollection<TileComponent> components => new(_components);
+        public IObservable<Tile> onTileChanged => onTileChangedSubject;
+        private readonly Subject<Tile> onTileChangedSubject = new();
         private List<TileComponent> _components = new();
 
         public Tile(string id, Position position, TileComponent[] components)
@@ -16,7 +20,20 @@ namespace JuhaKurisu.PiKAEngine.Logics
             this.id = id;
             this.position = position;
             this._components = components.ToList();
-            foreach (var component in _components) component.Initialize(this);
+            foreach (var component in _components)
+            {
+                component.Initialize(this);
+                component.onTileComponentChanged.Subscribe(_ => onTileChangedSubject.OnNext(this));
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach (var component in components)
+            {
+                component.Dispose();
+            }
+            onTileChangedSubject.Dispose();
         }
     }
 }
