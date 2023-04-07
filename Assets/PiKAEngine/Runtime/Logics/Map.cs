2239d3260f17a44571fb9aa42b63a99dfbfe6eed
KYapp <kukiyomenaisapp@gmail.com>
2023-04-07 16:27:09 +0900

    保持しているチャンクのタイルに変更があった場合呼ばれる処理を追加

--- Assets/PiKAEngine/Runtime/Logics/Map.cs
+++ Assets/PiKAEngine/Runtime/Logics/Map.cs
@@ -1,12 +1,16 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using UniRx;
 
 namespace JuhaKurisu.PiKAEngine.Logics
 {
-    public class Map
+    public class Map : IDisposable
     {
         public ReadOnlyDictionary<ChunkPosition, Chunk> chunks => new(_chunks);
         private readonly Dictionary<ChunkPosition, Chunk> _chunks;
+        private IObservable<Tile> onTileChanged => onTileChangedSubject;
+        public readonly Subject<Tile> onTileChangedSubject = new();
 
         public Map()
         {
@@ -15,7 +19,20 @@ namespace JuhaKurisu.PiKAEngine.Logics
 
         public bool TryAddChunk(Chunk chunk)
         {
-            return _chunks.TryAdd(chunk.position, chunk);
+            bool ret = _chunks.TryAdd(chunk.position, chunk);
+
+            chunk.onTileChanged.Subscribe(tile => onTileChangedSubject.OnNext(tile));
+
+            return ret;
+        }
+
+        public void Dispose()
+        {
+            onTileChangedSubject.Dispose();
+            foreach (var chunk in chunks.Values)
+            {
+                chunk.Dispose();
+            }
         }
     }
 }
