be28cc2c2b9df7ea288b67398beb930a5d621fde
KYapp <kukiyomenaisapp@gmail.com>
2023-04-07 16:18:59 +0900

    TileComponentに何か変更があったときに呼び出される処理を追加

--- Assets/PiKAEngine/Runtime/Logics/TileComponent.cs
+++ Assets/PiKAEngine/Runtime/Logics/TileComponent.cs
@@ -1,11 +1,15 @@
 using System;
+using UniRx;
 
 namespace JuhaKurisu.PiKAEngine.Logics
 {
     [Serializable]
-    public abstract class TileComponent
+    public abstract class TileComponent : IDisposable
     {
+        public IObservable<TileComponent> onTileComponentChanged => onTileComponentChangedSubject;
+        private readonly Subject<TileComponent> onTileComponentChangedSubject = new();
         private Tile tile;
+
         public void Initialize(Tile tile)
         {
             if (tile != null) return;
@@ -13,5 +17,10 @@ namespace JuhaKurisu.PiKAEngine.Logics
         }
         public abstract void Update();
         public abstract TileComponent Copy();
+
+        public void Dispose()
+        {
+            onTileComponentChangedSubject.Dispose();
+        }
     }
 }
