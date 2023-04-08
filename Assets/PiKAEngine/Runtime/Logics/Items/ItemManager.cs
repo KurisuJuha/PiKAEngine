0cb6050bf568ad02dd6f2123f1eec8dcfd438589
KYapp <kukiyomenaisapp@gmail.com>
2023-04-08 22:02:59 +0900

    ItemManagerに登録はいらないため、削除

--- Assets/PiKAEngine/Runtime/Logics/Items/ItemManager.cs
+++ Assets/PiKAEngine/Runtime/Logics/Items/ItemManager.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Linq;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using UniRx;
 
@@ -8,13 +6,9 @@ namespace JuhaKurisu.PiKAEngine.Logics.Items
 {
     public class ItemManager
     {
-        public ReadOnlyCollection<Item> items => itemList.AsReadOnly();
-        private readonly List<Item> itemList = new();
         public readonly ReadOnlyCollection<ItemComponent> baseComponents;
         public IObservable<Unit> onUpdate => onUpdateSubject;
         private readonly Subject<Unit> onUpdateSubject = new();
-        private List<Item> addingItems = new();
-        private List<Item> removingItems = new();
 
         public ItemManager(ItemComponent[] baseComponents)
         {
@@ -24,37 +18,6 @@ namespace JuhaKurisu.PiKAEngine.Logics.Items
         public void Update()
         {
             onUpdateSubject.OnNext(Unit.Default);
-        }
-
-        private void ApplyItems()
-        {
-            // アイテムたちを追加
-            itemList.AddRange(addingItems);
-            addingItems.Clear();
-
-            // アイテムたちを削除
-            removingItems.Select(removingItem => itemList.Remove(removingItem));
-            removingItems.Clear();
-        }
-
-        public void AddItem(Item item)
-        {
-            addingItems.Add(item);
-        }
-
-        public void AddItems(Item[] items)
-        {
-            addingItems.AddRange(items);
-        }
-
-        public void RemoveItem(Item item)
-        {
-            removingItems.Add(item);
-        }
-
-        public void RemoveItems(Item[] items)
-        {
-            removingItems.AddRange(items);
         }
     }
 }
