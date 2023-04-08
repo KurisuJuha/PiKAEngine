2aeee954106bb200e2b3545ece306cd908b4261d
KYapp <kukiyomenaisapp@gmail.com>
2023-04-08 22:04:53 +0900

    Tileがmapにアップデートを登録するようにした

--- Assets/PiKAEngine/Runtime/Logics/Maps/Tile.cs
+++ Assets/PiKAEngine/Runtime/Logics/Maps/Tile.cs
@@ -11,17 +11,33 @@ namespace JuhaKurisu.PiKAEngine.Logics.Maps
         public readonly ReadOnlyCollection<TileComponent> components;
         public IObservable<Tile> onTileChanged => onTileChangedSubject;
         private readonly Subject<Tile> onTileChangedSubject = new();
+        public IObservable<Tile> onUpdate => onUpdateSubject;
+        private readonly Subject<Tile> onUpdateSubject = new();
+        private readonly Map map;
+        private IDisposable tileUpdateDisposable;
 
-        public Tile(Position position, TileComponent[] components, TileSettings tileSettings)
+        public Tile(Position position, TileComponent[] components, Map map)
         {
             this.position = position;
-            this.components = new(components.Concat(tileSettings.baseComponents).ToArray());
+            this.components = new(components.Concat(map.baseComponents).ToArray());
+            this.map = map;
 
             foreach (var component in components)
             {
                 component.Initialize(this);
                 component.onTileComponentChanged.Subscribe(_ => onTileChangedSubject.OnNext(this));
             }
+        }
+
+        public void SubscribeTileUpdate()
+        {
+            if (tileUpdateDisposable is not null) return;
+            tileUpdateDisposable = map.onUpdate.Subscribe(_ => TileUpdate());
+        }
+
+        private void TileUpdate()
+        {
+            onUpdateSubject.OnNext(this);
         }
 
         public void Dispose()
