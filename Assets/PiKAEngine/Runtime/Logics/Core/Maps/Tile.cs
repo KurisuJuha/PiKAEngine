8fa800240b74b2c38903a215d6a0ebd48a8ea2e9
KYapp <kukiyomenaisapp@gmail.com>
2023-04-16 21:24:30 +0900

    tileにstartと言う概念を追加

--- Assets/PiKAEngine/Runtime/Logics/Core/Maps/Tile.cs
+++ Assets/PiKAEngine/Runtime/Logics/Core/Maps/Tile.cs
@@ -13,8 +13,11 @@ namespace JuhaKurisu.PiKAEngine.Logics.Core.Maps
         private readonly Subject<Tile> onTileChangedSubject = new();
         public IObservable<Tile> onUpdate => onUpdateSubject;
         private readonly Subject<Tile> onUpdateSubject = new();
+        public IObservable<Tile> onStart => onStartSubject;
+        private readonly Subject<Tile> onStartSubject = new();
         private readonly Map map;
         private IDisposable tileUpdateDisposable;
+        private IDisposable tileStartDisposable;
 
         public Tile(Position position, TileComponent[] components, Map map)
         {
@@ -27,12 +30,28 @@ namespace JuhaKurisu.PiKAEngine.Logics.Core.Maps
                 component.Initialize(this);
                 component.onTileComponentChanged.Subscribe(_ => onTileChangedSubject.OnNext(this));
             }
+
+            tileStartDisposable = map.onUpdate.Subscribe(_ =>
+            {
+                TileStart();
+                tileStartDisposable.Dispose();
+            });
         }
 
         public void SubscribeTileUpdate()
         {
             if (tileUpdateDisposable is not null) return;
             tileUpdateDisposable = map.onUpdate.Subscribe(_ => TileUpdate());
+        }
+
+        public void UnsubscribeTileUpdate()
+        {
+            tileUpdateDisposable?.Dispose();
+        }
+
+        private void TileStart()
+        {
+            onStartSubject.OnNext(this);
         }
 
         private void TileUpdate()
@@ -46,6 +65,8 @@ namespace JuhaKurisu.PiKAEngine.Logics.Core.Maps
             {
                 component.Dispose();
             }
+            tileUpdateDisposable?.Dispose();
+            tileStartDisposable?.Dispose();
             onTileChangedSubject.Dispose();
         }
     }
