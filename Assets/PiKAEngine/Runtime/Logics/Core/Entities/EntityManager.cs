7eec5b133c1fd41ec920fa58fae7570e0c8dd4c3
KYapp <kukiyomenaisapp@gmail.com>
2023-04-16 21:08:05 +0900

    EntityManagerがEntityを保持する必要はないので削除

--- Assets/PiKAEngine/Runtime/Logics/Core/Entities/EntityManager.cs
+++ Assets/PiKAEngine/Runtime/Logics/Core/Entities/EntityManager.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Linq;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using UniRx;
 
@@ -8,13 +6,9 @@ namespace JuhaKurisu.PiKAEngine.Logics.Core.Entities
 {
     public class EntityManager
     {
-        public ReadOnlyCollection<Entity> entities => entityList.AsReadOnly();
-        private readonly List<Entity> entityList = new();
         public readonly ReadOnlyCollection<EntityComponent> baseComponents;
         public IObservable<Unit> onUpdate => onUpdateSubject;
         private readonly Subject<Unit> onUpdateSubject = new();
-        private List<Entity> addingEntities = new();
-        private List<Entity> removingEntities = new();
 
         public EntityManager(params EntityComponent[] baseComponents)
         {
@@ -23,39 +17,7 @@ namespace JuhaKurisu.PiKAEngine.Logics.Core.Entities
 
         public void Update()
         {
-            ApplyEntities();
             onUpdateSubject.OnNext(Unit.Default);
-        }
-
-        private void ApplyEntities()
-        {
-            // エンティティたちを追加
-            entityList.AddRange(addingEntities);
-            addingEntities.Clear();
-
-            // エンティティたちを削除
-            removingEntities.Select(removingEntity => entityList.Remove(removingEntity));
-            removingEntities.Clear();
-        }
-
-        public void AddEntity(Entity entity)
-        {
-            addingEntities.Add(entity);
-        }
-
-        public void AddEntities(Entity[] entities)
-        {
-            addingEntities.AddRange(entities);
-        }
-
-        public void RemoveEntity(Entity entity)
-        {
-            removingEntities.Remove(entity);
-        }
-
-        public void RemoveEntities(Entity[] entities)
-        {
-            removingEntities.AddRange(entities);
         }
     }
 }
