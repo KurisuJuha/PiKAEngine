90269449188b84a35d4725995f2f6b0ba1dff801
KYapp <kukiyomenaisapp@gmail.com>
2023-04-16 22:24:34 +0900

    tilecomponentにstartとupdateを追加

--- Assets/PiKAEngine/Runtime/Logics/Core/Maps/TileComponent.cs
+++ Assets/PiKAEngine/Runtime/Logics/Core/Maps/TileComponent.cs
@@ -9,19 +9,39 @@ namespace JuhaKurisu.PiKAEngine.Logics.Core.Maps
         public IObservable<TileComponent> onTileComponentChanged => onTileComponentChangedSubject;
         private readonly Subject<TileComponent> onTileComponentChangedSubject = new();
         protected Tile tile { get; private set; }
+        private IDisposable componentUpdateDisposable;
+        private IDisposable componentStartDisposable;
 
         public void Initialize(Tile tile)
         {
             if (this.tile != null) return;
             this.tile = tile;
+            componentStartDisposable = tile.onStart.Subscribe(_ =>
+            {
+                ComponentStart();
+                componentStartDisposable.Dispose();
+            });
         }
 
-        public abstract void Update();
+        public void SubscribeComponentUpdate()
+        {
+            componentUpdateDisposable = tile.onUpdate.Subscribe(_ => ComponentUpdate());
+        }
+
+        public void UnsubscribeComponentUpdate()
+        {
+            componentUpdateDisposable?.Dispose();
+        }
+
+        protected virtual void ComponentStart() { }
+        protected virtual void ComponentUpdate() { }
         public abstract TileComponent Copy();
 
         public void Dispose()
         {
             onTileComponentChangedSubject.Dispose();
+            componentUpdateDisposable?.Dispose();
+            componentStartDisposable?.Dispose();
         }
     }
 }
