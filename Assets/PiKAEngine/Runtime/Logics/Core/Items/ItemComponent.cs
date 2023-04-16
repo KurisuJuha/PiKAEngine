094ba6ef84be60a093c2e45fbda989390ae27147
KYapp <kukiyomenaisapp@gmail.com>
2023-04-16 22:18:00 +0900

    ItemComponentにUpdateとStartを追加

--- Assets/PiKAEngine/Runtime/Logics/Core/Items/ItemComponent.cs
+++ Assets/PiKAEngine/Runtime/Logics/Core/Items/ItemComponent.cs
@@ -9,19 +9,35 @@ namespace JuhaKurisu.PiKAEngine.Logics.Core.Items
         public IObservable<ItemComponent> onItemComponentChanged => onItemComponentChangedSubject;
         private readonly Subject<ItemComponent> onItemComponentChangedSubject = new();
         protected Item item { get; private set; }
+        private IDisposable componentUpdateDisposable;
+        private IDisposable componentStartDisposable;
 
         public void Initialize(Item item)
         {
             if (this.item != null) return;
             this.item = item;
+            componentStartDisposable = item.onStart.Subscribe(_ => ComponentStart());
         }
 
-        public abstract void Update();
+        public void SubscribeComponentUpdate()
+        {
+            componentUpdateDisposable = item.onUpdate.Subscribe(_ => ComponentUpdate());
+        }
+
+        public void UnsubscribeComponentUpdate()
+        {
+            componentUpdateDisposable?.Dispose();
+        }
+
+        protected virtual void ComponentStart() { }
+        protected virtual void ComponentUpdate() { }
         public abstract ItemComponent Copy();
 
         public void Dispose()
         {
             onItemComponentChangedSubject.Dispose();
+            componentUpdateDisposable?.Dispose();
+            componentStartDisposable?.Dispose();
         }
     }
 }
