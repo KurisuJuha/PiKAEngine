b7f6a39175d581936c8752f1ccaaa9656bcc1ff6
KYapp <kukiyomenaisapp@gmail.com>
2023-05-20 16:23:26 +0900

    riderのautoformatを使用

--- PiKAEngine/Core/Items/ItemManager.cs
+++ PiKAEngine/Core/Items/ItemManager.cs
@@ -1,82 +1,84 @@
-namespace PiKAEngine.Core.Items
+namespace PiKAEngine.Core.Items;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class ItemManager : IDisposable
 {
-    // ReSharper disable once ClassNeverInstantiated.Global
-    public class ItemManager : IDisposable
+    private readonly HashSet<Item> activeItems;
+    private readonly List<Item> addingItems;
+    private readonly List<Item> initializingItems;
+    private readonly HashSet<Item> items;
+    private readonly List<Item> removingItems;
+
+    public ItemManager()
     {
-        private readonly HashSet<Item> items;
-        private readonly HashSet<Item> activeItems;
-        private readonly List<Item> addingItems;
-        private readonly List<Item> removingItems;
-        private readonly List<Item> initializingItems;
+        items = new HashSet<Item>();
+        activeItems = new HashSet<Item>();
+        addingItems = new List<Item>();
+        removingItems = new List<Item>();
+        initializingItems = new List<Item>();
+    }
 
-        public ItemManager()
+    public void Dispose()
+    {
+        foreach (var item in items) item.Dispose();
+    }
+
+    public void AddItemOnNextFrame(Item item)
+    {
+        addingItems.Add(item);
+    }
+
+    public void RemoveItemOnNextFrame(Item item)
+    {
+        removingItems.Add(item);
+    }
+
+    public void ActivateItem(Item item)
+    {
+        if (!items.Contains(item)) AddItemOnNextFrame(item);
+        activeItems.Add(item);
+    }
+
+    public void DeactivateItem(Item item)
+    {
+        activeItems.Remove(item);
+    }
+
+    public void Update()
+    {
+        // アイテムの追加処理
+        var addingItemsCache = new List<Item>(addingItems);
+        addingItems.Clear();
+        foreach (var item in addingItemsCache)
         {
-            items = new HashSet<Item>();
-            activeItems = new HashSet<Item>();
-            addingItems = new List<Item>();
-            removingItems = new List<Item>();
-            initializingItems = new List<Item>();
+            items.Add(item);
+            initializingItems.Add(item);
         }
 
-        public void AddItemOnNextFrame(Item item)
-            => addingItems.Add(item);
-
-        public void RemoveItemOnNextFrame(Item item)
-            => removingItems.Add(item);
-
-        public void ActivateItem(Item item)
+        // アイテムの削除処理
+        var removingItemsCache = new List<Item>(removingItems);
+        removingItems.Clear();
+        foreach (var item in removingItemsCache)
         {
-            if (!items.Contains(item)) AddItemOnNextFrame(item);
-            activeItems.Add(item);
+            item.OnDestory();
+            item.Dispose();
+            items.Remove(item);
+            activeItems.Remove(item);
+            initializingItems.Remove(item);
         }
 
-        public void DeactivateItem(Item item)
-            => activeItems.Remove(item);
+        // アイテムのinitialize処理
+        var initializingItemsCache = new List<Item>(initializingItems);
+        initializingItems.Clear();
+        foreach (var item in initializingItemsCache)
+            item.Initialize();
 
-        public void Update()
-        {
-            // アイテムの追加処理
-            var addingItemsCache = new List<Item>(addingItems);
-            addingItems.Clear();
-            foreach (var item in addingItemsCache)
-            {
-                items.Add(item);
-                initializingItems.Add(item);
-            }
+        // アイテムのstart処理
+        foreach (var item in initializingItemsCache)
+            item.Start();
 
-            // アイテムの削除処理
-            var removingItemsCache = new List<Item>(removingItems);
-            removingItems.Clear();
-            foreach (var item in removingItemsCache)
-            {
-                item.OnDestory();
-                item.Dispose();
-                items.Remove(item);
-                activeItems.Remove(item);
-                initializingItems.Remove(item);
-            }
-
-            // アイテムのinitialize処理
-            var initializingItemsCache = new List<Item>(initializingItems);
-            initializingItems.Clear();
-            foreach (var item in initializingItemsCache)
-                item.Initialize();
-
-            // アイテムのstart処理
-            foreach (var item in initializingItemsCache)
-                item.Start();
-
-            // アイテムのupdate処理
-            foreach (var item in activeItems)
-                item.Update();
-        }
-
-        public void Dispose()
-        {
-            foreach (var item in items)
-            {
-                item.Dispose();
-            }
-        }
+        // アイテムのupdate処理
+        foreach (var item in activeItems)
+            item.Update();
     }
 }
