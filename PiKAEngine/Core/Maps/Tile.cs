8da0c8c3e419a80f3b2073bff3a42b8ae0d6cbd2
KYapp <kukiyomenaisapp@gmail.com>
2023-05-20 16:32:44 +0900

    riderのautoformatを使用

--- PiKAEngine/Core/Maps/Tile.cs
+++ PiKAEngine/Core/Maps/Tile.cs
@@ -1,26 +1,26 @@
-namespace PiKAEngine.Core.Maps
+namespace PiKAEngine.Core.Maps;
+
+public abstract class Tile : IDisposable
 {
-    public abstract class Tile : IDisposable
+    // ReSharper disable once MemberCanBePrivate.Global
+    public readonly TileManager tileManager;
+
+    protected Tile(TileManager tileManager)
     {
-        // ReSharper disable once MemberCanBePrivate.Global
-        public readonly TileManager tileManager;
-        public Chunk? chunk { get; private set; }
-        public MapPosition position { get; private set; }
-
-        protected Tile(TileManager tileManager)
-        {
-            this.tileManager = tileManager;
-        }
-
-        public void InitializeBase(MapPosition mapPosition)
-        {
-            this.position = mapPosition;
-            chunk = tileManager.chunkDictionary[mapPosition.chunkPosition];
-        }
-
-        public abstract void Initialize();
-        public abstract void Start();
-        public abstract void Update();
-        public abstract void Dispose();
+        this.tileManager = tileManager;
     }
+
+    public Chunk? chunk { get; private set; }
+    public MapPosition position { get; private set; }
+    public abstract void Dispose();
+
+    public void InitializeBase(MapPosition mapPosition)
+    {
+        position = mapPosition;
+        chunk = tileManager.chunkDictionary[mapPosition.chunkPosition];
+    }
+
+    public abstract void Initialize();
+    public abstract void Start();
+    public abstract void Update();
 }
