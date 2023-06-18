032786e0ef697a6b0b8a597b43239be8e7f587f4
KYapp <kukiyomenaisapp@gmail.com>
2023-06-18 20:20:46 +0900

    ItemManagerのRename

--- PiKAEngine/Core/Items/ItemManager.cs
+++ PiKAEngine/Core/Items/ItemManager.cs
@@ -5,75 +5,75 @@ namespace PiKATools.Engine.Core.Items;
 // ReSharper disable once ClassNeverInstantiated.Global
 public class ItemManager : IDisposable
 {
-    private readonly HashSet<Item> activeItems;
-    private readonly List<Item> addingItems;
-    private readonly List<Item> initializingItems;
-    private readonly HashSet<Item> items;
-    public readonly Kettle kettle;
-    private readonly List<Item> removingItems;
+    private readonly HashSet<Item> _activeItems;
+    private readonly List<Item> _addingItems;
+    private readonly List<Item> _initializingItems;
+    private readonly HashSet<Item> _items;
+    private readonly List<Item> _removingItems;
+    public readonly Kettle Kettle;
 
     public ItemManager(Kettle kettle)
     {
-        this.kettle = kettle;
-        items = new HashSet<Item>();
-        activeItems = new HashSet<Item>();
-        addingItems = new List<Item>();
-        removingItems = new List<Item>();
-        initializingItems = new List<Item>();
+        Kettle = kettle;
+        _items = new HashSet<Item>();
+        _activeItems = new HashSet<Item>();
+        _addingItems = new List<Item>();
+        _removingItems = new List<Item>();
+        _initializingItems = new List<Item>();
     }
 
     public void Dispose()
     {
-        foreach (var item in items) item.Dispose();
+        foreach (var item in _items) item.Dispose();
     }
 
     public void AddItemOnNextFrame(Item item)
     {
-        addingItems.Add(item);
+        _addingItems.Add(item);
     }
 
     public void RemoveItemOnNextFrame(Item item)
     {
-        removingItems.Add(item);
+        _removingItems.Add(item);
     }
 
     public void ActivateItem(Item item)
     {
-        if (!items.Contains(item)) AddItemOnNextFrame(item);
-        activeItems.Add(item);
+        if (!_items.Contains(item)) AddItemOnNextFrame(item);
+        _activeItems.Add(item);
     }
 
     public void DeactivateItem(Item item)
     {
-        activeItems.Remove(item);
+        _activeItems.Remove(item);
     }
 
     public void Update()
     {
         // アイテムの追加処理
-        var addingItemsCache = new List<Item>(addingItems);
-        addingItems.Clear();
+        var addingItemsCache = new List<Item>(_addingItems);
+        _addingItems.Clear();
         foreach (var item in addingItemsCache)
         {
-            items.Add(item);
-            initializingItems.Add(item);
+            _items.Add(item);
+            _initializingItems.Add(item);
         }
 
         // アイテムの削除処理
-        var removingItemsCache = new List<Item>(removingItems);
-        removingItems.Clear();
+        var removingItemsCache = new List<Item>(_removingItems);
+        _removingItems.Clear();
         foreach (var item in removingItemsCache)
         {
             item.OnDestory();
             item.Dispose();
-            items.Remove(item);
-            activeItems.Remove(item);
-            initializingItems.Remove(item);
+            _items.Remove(item);
+            _activeItems.Remove(item);
+            _initializingItems.Remove(item);
         }
 
         // アイテムのinitialize処理
-        var initializingItemsCache = new List<Item>(initializingItems);
-        initializingItems.Clear();
+        var initializingItemsCache = new List<Item>(_initializingItems);
+        _initializingItems.Clear();
         foreach (var item in initializingItemsCache)
             item.Initialize();
 
@@ -82,7 +82,7 @@ public class ItemManager : IDisposable
             item.Start();
 
         // アイテムのupdate処理
-        foreach (var item in activeItems)
+        foreach (var item in _activeItems)
             item.Update();
     }
 }
