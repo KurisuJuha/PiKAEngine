f489cbfcef8ad747dbbf21ac060e3b017ea9a34e
Juha <kukiyomenaisapp@gmail.com>
2023-07-14 15:02:59 +0900

    AddItem

--- PiKAEngine.Core/InventorySystem/InventoryGrid.cs
+++ PiKAEngine.Core/InventorySystem/InventoryGrid.cs
@@ -2,12 +2,36 @@
 
 public abstract class InventoryGrid<T> : IInventoryGrid<T>
 {
-    private readonly IInventorySettings _inventorySettings;
-    private readonly List<T> _items;
+    private readonly int _gridMaxAmount;
+    private readonly IInventorySettings<T> _inventorySettings;
+    private readonly List<T> _items = new();
 
-    protected InventoryGrid(IInventorySettings inventorySettings)
+    protected InventoryGrid(IInventorySettings<T> inventorySettings, int gridMaxAmount)
     {
         _inventorySettings = inventorySettings;
-        _items = new List<T>();
+        _gridMaxAmount = gridMaxAmount;
+    }
+
+    private bool IsAddableItem(T addingItem)
+    {
+        // 数量がそもそもマックスならアウト
+        if (_items.Count >= _inventorySettings.GetMaxItemAmount(addingItem)) return false;
+        if (_items.Count >= _gridMaxAmount) return false;
+
+        // 数量が1以上かつ、一つ目のアイテムと同じ種類ではないならアウト
+        if (_items.Count != 0 && !_inventorySettings.AreSameItems(_items[0], addingItem)) return false;
+
+        return true;
+    }
+
+    public bool TryAddItem(T addingItem)
+    {
+        if (!IsAddableItem(addingItem)) return false;
+
+        _items.Add(addingItem);
+
+        return true;
+    }
+
     }
 }
