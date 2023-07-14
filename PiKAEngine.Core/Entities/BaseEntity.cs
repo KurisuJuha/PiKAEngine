bc05f55dfdda1bbff30e1b1f7d417c12f0dd5900
Juha <kukiyomenaisapp@gmail.com>
2023-07-14 14:23:05 +0900

    EntityをBaseEntityに変更

---
+++ PiKAEngine.Core/Entities/BaseEntity.cs
@@ -0,0 +1,36 @@
+using PiKATools.Engine.Core.DebugSystem;
+
+namespace PiKATools.Engine.Core.Entities;
+
+public abstract class BaseEntity<TEntity> : IDisposable
+    where TEntity : BaseEntity<TEntity>
+{
+    private readonly BaseEntityManager<TEntity> _baseEntityManager;
+    public readonly Guid Id;
+
+    protected BaseEntity(BaseEntityManager<TEntity> baseEntityManager, bool registerEntityManager = true)
+    {
+        _baseEntityManager = baseEntityManager;
+        Id = new Guid();
+
+        if (registerEntityManager) baseEntityManager.AddEntityOnNextFrame((TEntity)this);
+    }
+
+    public Kettle Kettle => _baseEntityManager.Kettle;
+
+    public abstract void Dispose();
+
+    protected void Activate()
+    {
+        _baseEntityManager.ActivateEntity((TEntity)this);
+    }
+
+    protected void Deactivate()
+    {
+        _baseEntityManager.DeactivateEntity((TEntity)this);
+    }
+
+    public abstract void Initialize();
+    public abstract void Start();
+    public abstract void Update();
+}
