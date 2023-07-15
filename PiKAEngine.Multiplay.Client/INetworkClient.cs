cfe6af117854be74f70dabb60d02a26be54219be
Juha <kukiyomenaisapp@gmail.com>
2023-07-15 13:18:33 +0900

    全部消した

--- PiKAEngine.Multiplay.Client/INetworkClient.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PiKAEngine.MultiPlay.Client;
-
-public interface INetworkClient
-{
-    IObservable<IEnumerable<byte>> OnMessage { get; }
-    void SendMessage(IEnumerable<byte> message);
-}
--- PiKAEngine.Multiplay.Server/INetworkServer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PiKAEngine.MultiPlay.Server;
-
-public interface INetworkServer
-{
-    IObservable<(Guid, IEnumerable<byte>)> OnMessage { get; }
-    IObservable<Guid> OnConnected { get; }
-    IObservable<Guid> OnDisconnected { get; }
-    void SendMessage(Guid id, IEnumerable<byte> message);
-}
