acdc4ce92cdf121a44c7cbec7c50314510a477b9
Juha <kukiyomenaisapp@gmail.com>
2023-07-31 21:16:10 +0900

    ComponentsAddableEntityBaseのガワを定義

---
+++ PiKAEngine.Entities/ComponentsAddableEntityBase.cs
@@ -0,0 +1,10 @@
+﻿namespace PiKAEngine.Entities;
+
+public abstract class ComponentsAddableEntityBase<TEntity> : EntityBase<TEntity>
+    where TEntity : EntityBase<TEntity>
+{
+    protected ComponentsAddableEntityBase(EntityManagerBase<TEntity> entityManagerBase,
+        bool registerEntityManager = true) : base(entityManagerBase, registerEntityManager)
+    {
+    }
+}
