2b1dea6851d7bd20002c7f3de5d1f4e6cfc73214
Juha <kukiyomenaisapp@gmail.com>
2023-09-06 15:47:05 +0900

    BenchMark

--- PiKAEngine.ColliderSystem.Sample/Program.cs
+++ PiKAEngine.ColliderSystem.Sample/Program.cs
@@ -3,17 +3,33 @@
 using PiKAEngine.ColliderSystem;
 using PiKAEngine.Mathematics;
 
-var world = new ColliderWorld<int>(new WorldTransform(8, new FixVector2(-4096, -4096), new FixVector2(8192, 8192)));
-var collider =
-    new RectCollider<int>(12, new RectColliderTransform(new FixVector2(5, 5), FixVector2.One, Fix64.Zero), world);
-var collider2 =
-    new RectCollider<int>(12, new RectColliderTransform(new FixVector2(5, 5), FixVector2.One, Fix64.Zero), world, true);
+var rectColliderTransformA = new RectColliderTransform(new FixVector2(1, 2), new FixVector2(4, 4), Fix64.Zero);
+var rectColliderTransformB = new RectColliderTransform(new FixVector2(100, 100), new FixVector2(3, 4), Fix64.Zero);
 
-collider.Register();
-collider2.Register();
+var count = 1000000;
+var startTime = DateTime.Now;
+var endTime = DateTime.Now;
+var time = endTime - startTime;
 
-world.PointCast(new FixVector2(5, 5));
-world.Check();
+startTime = DateTime.Now;
 
-Console.WriteLine($"ContactingColliders {world.ContactingColliders.Count}");
-Console.WriteLine($"RaycastContactingColliders {world.RayCastContactingColliders.Count}");
+for (var i = 0; i < count; i++) rectColliderTransformA.Detect(rectColliderTransformB.LeftBottomPosition);
+
+endTime = DateTime.Now;
+
+time = endTime - startTime;
+
+Console.WriteLine($"pointDetect totalMilliSeconds {time.TotalMilliseconds}ms");
+Console.WriteLine($"pointDetect total/count {time.TotalMilliseconds / count}ms");
+
+startTime = DateTime.Now;
+
+for (var i = 0; i < count; i++)
+    rectColliderTransformA.Detect(rectColliderTransformB.LeftBottomPosition, rectColliderTransformB.RightTopPosition);
+
+endTime = DateTime.Now;
+
+time = endTime - startTime;
+
+Console.WriteLine($"pointDetect totalMilliSeconds {time.TotalMilliseconds}ms");
+Console.WriteLine($"pointDetect total/count {time.TotalMilliseconds / count}ms");
