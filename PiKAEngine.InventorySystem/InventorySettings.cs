fa730f46ff4d92f9d39ffc3a3b7da8b11245d881
Juha <kukiyomenaisapp@gmail.com>
2023-09-08 15:00:57 +0900

    InventorySettingsの実際の型を用意

---
+++ PiKAEngine.InventorySystem/InventorySettings.cs
@@ -0,0 +1,23 @@
+﻿namespace PiKAEngine.InventorySystem;
+
+public class InventorySettings<T> : IInventorySettings<T>
+{
+    private readonly Func<T, T, bool> _areSameItems;
+    private readonly Func<T, int> _getMaxItemAmount;
+
+    public InventorySettings(Func<T, T, bool> areSameItems, Func<T, int> getMaxItemAmount)
+    {
+        _areSameItems = areSameItems;
+        _getMaxItemAmount = getMaxItemAmount;
+    }
+
+    public int GetMaxItemAmount(T item)
+    {
+        return _getMaxItemAmount(item);
+    }
+
+    public bool AreSameItems(T item1, T item2)
+    {
+        return _areSameItems(item1, item2);
+    }
+}
